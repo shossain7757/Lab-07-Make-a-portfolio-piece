3ed061edc6b01638af1b5b6146abdb119380c211
shossain7757 <71143213+shossain7757@users.noreply.github.com>
2022-04-05 18:52:42 -0400

    Update Data_Clean&Wrangling.R

--- R/Data_Clean&Wrangling.R
+++ R/Data_Clean&Wrangling.R
@@ -1,7 +1,26 @@
 library(tidyverse)
 
-data <- read.table(here::here('Data','DS0001','08001-0001-Codebook.txt'))
+data <- read.table(here::here('Data','SEER_breast_cancer.csv'), header = TRUE, sep = ',')
 
+# Data Cleaning
+
+## Drop Repetitive Columns 
+
+
+
+## Rename Some Columns
+
+data |>
+  rename(Age_group = Age.recode.with..1.year.olds,
+         Race = Race.recode..White..Black..Other.,
+         Cancer_site_of_AYA = AYA.site.recode.2020.Revision,
+         Tumor_type = ICCC.site.recode.3rd.edition.IARC.2017,
+         Primary_site = Primary.Site...labeled,
+         Histology_code = Histologic.Type.ICD.O.3,
+         Cancer_grade = Grade..thru.2017.,
+         ) |>
+  names()
+  
 
 
 
