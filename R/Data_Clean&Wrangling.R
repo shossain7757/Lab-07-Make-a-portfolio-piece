85d45ab2020d781c4a5ea2482e7bb4d158b6a0b3
shossain7757 <71143213+shossain7757@users.noreply.github.com>
2022-04-18 23:36:33 -0400

    Update Data_Clean&Wrangling.R

--- R/Data_Clean&Wrangling.R
+++ R/Data_Clean&Wrangling.R
@@ -6,20 +6,31 @@ data <- read.table(here::here('Data','SEER_breast_cancer.csv'), header = TRUE, s
 
 ## Drop Repetitive Columns 
 
-names(data)
+clean_data <- data |>
+  select(-Primary.Site,-Histologic.Type.ICD.O.3,-ICD.O.3.Hist.behav,-ICD.O.3.Hist.behav..malignant,
+         -ICCC.site.recode.3rd.edition.IARC.2017,-Site.recode.ICD.O.3.WHO.2008..for.SIRs.,
+         -ICCC.site.recode.ICD.O.3.WHO.2008,-ICCC.site.rec.extended.ICD.O.3.WHO.2008,
+         -Schema.ID..2018..,-AJCC.ID..2018..,-EOD.Schema.ID.Recode..2010..,
+         -Site.recode...rare.tumors,-AYA.site.recode.WHO.2008,-RX.Summ..Surg.Prim.Site..1998..,
+         -RX.Summ..Surg.Oth.Reg.Dis..2003..,-AYA.site.recode.2020.Revision,
+         -ICCC.site.recode.extended.3rd.edition.IARC.2017,
+         -SEER.other.cause.of.death.classification,-COD.to.site.recode)
+  
+
 
 ## Rename Some Columns
 
-data |>
+clean_data <- clean_data |>
   rename(Age_group = Age.recode.with..1.year.olds,
          Race = Race.recode..White..Black..Other.,
-         Cancer_site_of_AYA = AYA.site.recode.2020.Revision,
-         Tumor_type = ICCC.site.recode.3rd.edition.IARC.2017,
          Primary_site = Primary.Site...labeled,
-         Histology_code = Histologic.Type.ICD.O.3,
          Cancer_grade = Grade..thru.2017.,
-         ) |>
-  names()
+         Cell_behavior = Behavior.recode.for.analysis,
+         Cancer_grade_clinical = Grade.Clinical..2018..,
+         Cancer_grade_pathological = Grade.Pathological..2018..,
+         Histology_Type = Histology.recode...broad.groupings,
+         Reason_for_no_surgery = Reason.no.cancer.directed.surgery) 
+  
   
 
 
